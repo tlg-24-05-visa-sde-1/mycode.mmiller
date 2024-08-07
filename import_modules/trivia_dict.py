95d2f52a26c5f7489b8e2329fddee2609d970e27
mnmilleratx <MeganM@tlgcohort.com>
2024-08-07 20:40:25 +0000

    studying for logic

---
+++ import_modules/trivia_dict.py
@@ -0,0 +1,16 @@
+import html
+
+trivia= {
+         "category": "Entertainment: Film",
+         "type": "multiple",
+         "question": "Which of the following is NOT a quote from the 1942 film Casablanca? ",
+         "correct_answer": "&quot;Frankly, my dear, I don&#039;t give a damn.&quot;",
+         "incorrect_answers": [
+             "&quot;Here&#039;s lookin&#039; at you, kid.&quot;",
+             "&ldquo;Of all the gin joints, in all the towns, in all the world, she walks into mine&hellip;&rdquo;",
+             "&quot;Round up the usual suspects.&quot;"
+            ]
+        }
+
+def main():
+
